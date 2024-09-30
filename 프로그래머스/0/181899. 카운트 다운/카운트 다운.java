d29b7ceddc7e52b01f44f17c706debf247d6a0fa
Yunji0000 <qkrdbswl00@naver.com>
2024-09-30 09:06:49 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer;
+        answer = new int[start_num-end_num+1];
+        
+        for(int i=0; start_num>=end_num; i++){
+            answer[i] = start_num;
+            start_num--;
+        }
+        
+        return answer;
+    }
+}
