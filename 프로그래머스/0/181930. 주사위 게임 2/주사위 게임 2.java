3efb9de5c72a6b021ce75827266b665518f8fa1d
Yunji0000 <qkrdbswl00@naver.com>
2024-09-30 09:03:25 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.10 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -6,14 +6,17 @@ class Solution {
          if (a != b && b != c && a != c) {
             // 모든 숫자가 다를 때
             return sum;
+             
         } else if ((a == b && b != c) || (a == c && a != b) || (b == c && a != b)) {
             // 두 숫자가 같고 나머지 하나가 다를 때
-            return sum * ((int)Math.pow(a, 2) + (int)Math.pow(b, 2) + (int)Math.pow(c, 2));
+           
+             return sum * (int)(Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
         } else if (a == b && b == c) {
             // 모든 숫자가 같을 때
             return sum * ((int)Math.pow(a, 2) + (int)Math.pow(b, 2) + (int)Math.pow(c, 2)) * ((int)Math.pow(a, 3) + (int)Math.pow(b, 3) + (int)Math.pow(c, 3));
         }
-      //  return answer;
-        return 0;
+        
+      return sum;
+        //return 0;
     }
 }
