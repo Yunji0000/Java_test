848f5a5e682faa799893647da568168c3c2e1d05
Yunji0000 <qkrdbswl00@naver.com>
2024-09-30 09:03:02 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        int answer = 0;
+        
+        if(ineq.equals("<")){
+            if(eq.equals("=")){
+                if(n <= m)
+                    return 1;
+            }
+            else{ //eq가 =일때
+                if(n < m)
+                    return 1;
+            }
+        }
+        else if(ineq.equals(">")){
+            if(eq.equals("=")) {
+                if( n >= m)
+                    return 1;
+            }
+            else {//eq가 !일때
+                if(n > m)
+                    return 1;
+            }
+        }
+        return answer;
+    }
+}
