6863be8fa5b7243afd508a4fe16d973c7c57e7c5
Yunji0000 <qkrdbswl00@naver.com>
2024-10-24 12:29:40 +0900

    [level 1] Title: 모의고사, Time: 0.08 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,29 +1,42 @@
-#include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
-    int person1[] = {1, 2, 3, 4, 5};
-    int person2[] = {2, 1, 2, 3, 2, 4, 2, 5};
-    int person3[] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    int num1[] = {1, 2, 3, 4, 5}; 
+    int num2[] = {2, 1, 2, 3, 2, 4, 2, 5}; 
+    int num3[] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; 
     
-    vector<int> score(3, 0);
-    
-    for (int i = 0; i < answers.size(); i++) {
-        if (answers[i] == person1[i % 5]) score[0]++; 
-        if (answers[i] == person2[i % 8]) score[1]++; 
-        if (answers[i] == person3[i % 10]) score[2]++;
-    }
+    int score1 = 0;
+    int score2 = 0;
+    int score3 = 0;
 
-    int maxScore = max({score[0], score[1], score[2]});
-    vector<int> result;
-    for (int i = 0; i < 3; i++) {
-        if (score[i] == maxScore) {
-            result.push_back(i + 1); 
+    for (int i = 0; i < answers.size(); i++) {
+        
+        if (answers[i] == num1[i % 5]) {
+            score1++; 
+        }
+
+        if (answers[i] == num2[i % 8]) {
+            score2++; 
+        }
+
+        if (answers[i] == num3[i % 10]) {
+            score3++;
         }
     }
-    
-    return result;
+
+    vector<int> answer;
+
+    if (score1 >= score2 && score1 >= score3) {
+        answer.push_back(1); 
+    }
+    if (score2 >= score1 && score2 >= score3) {
+        answer.push_back(2);
+    }
+    if (score3 >= score1 && score3 >= score2) {
+        answer.push_back(3);
+    }
+
+    return answer;
 }
