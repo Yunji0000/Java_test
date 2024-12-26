1772448301018c98315f4fb2a8acafdb7cc335f1
Yunji0000 <qkrdbswl00@naver.com>
2024-12-26 19:48:31 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double a, b;
+    
+    cin >> a >> b;
+    
+    cout.precision(9);
+    cout << fixed;
+    
+    cout << a/b;
+    
+    
+    return 0;
+}
