f4b3c7be1d87a50d2d5faa5b553bdf90a06fbd1e
Yunji0000 <qkrdbswl00@naver.com>
2024-12-27 11:08:27 +0900

    [Bronze V] Title: 합, Time: 172 ms, Memory: 17548 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -4,16 +4,14 @@ import java.io.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        int sum = 0;
+        int n = sc.nextInt();
         
-        for(int i=0; i<=T; i++){
+        int sum=0;
+        for(int i=0; i<=n; i++){
             sum += i;
-            
         }
-        System.out.println(sum);
         
+        System.out.println(sum);
         sc.close();
     }
-    
 }
