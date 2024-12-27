e06353ab33ee121c4110f52a29c0a11f844043cc
Yunji0000 <qkrdbswl00@naver.com>
2024-12-27 22:54:50 +0900

    [Bronze III] Title: 최소, 최대, Time: 452 ms, Memory: 92200 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());  
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+      
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()); 
+        }
+        
+        int min = arr[0];
+        int max = arr[0];
+        
+        for(int i = 1; i < N; i++) {
+            min = Math.min(min, arr[i]);
+            max = Math.max(max, arr[i]);
+        }
+        
+        bw.write(min + " " + max);
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
