77eb039454501ed8347ffc67924ff56f56bae85b
Yunji0000 <qkrdbswl00@naver.com>
2024-12-27 09:31:39 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int T;
+    int A, B;
+    
+    cin >> T;
+    // int result;
+
+    // cin >> A >> B;
+
+    for (int i= 0; i <T; i++){
+        cin >> A >> B;
+        cout << A + B << "\n";
+        // result = A + B;
+    }
+
+    return 0;
+}
