ec9d7256193cad6b40eccf0beace1fdbe613c94a
Yunji0000 <qkrdbswl00@naver.com>
2025-01-12 23:05:21 +0900

    [Silver III] Title: N과 M (4), Time: 184 ms, Memory: 23588 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static int N, M;
+
+    public static void recur(int num, int start, Deque<Integer> deque, BufferedWriter bw) throws IOException {
+        if (num==M) {
+            for(int i = 0; i < M; i++) {
+                bw.write(deque.toArray()[i]+" ");
+            }
+            bw.write("\n");
+            
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            deque.addLast(i);
+            recur(num+1, i, deque, bw);
+            deque.removeLast();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        recur(0, 1, deque, bw);
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
