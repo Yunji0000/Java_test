81c3171fdc51c5354acdeb522efedab9f2db4e37
Yunji0000 <qkrdbswl00@naver.com>
2025-01-19 01:59:11 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 684 ms, Memory: 52440 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr,(o1, o2) -> {
+            if(o1[1]==o2[1]) {
+                return o1[0]-o2[0];
+            }
+            return o1[1]-o2[1];
+        });
+
+        for(int i = 0; i < N; i++) {
+            bw.write(arr[i][0] +" " + arr[i][1] +"\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+}
